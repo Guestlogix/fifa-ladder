2d9b3d4959697d6619319ee0c77d61dd4225118b
Hank Goddard <me@henrygoddard.com>
2018-09-13 19:42:07 -0400

    visibility

--- app/views.py
+++ app/views.py
@@ -72,6 +72,7 @@ def get_games():
 @app.route('/games', methods=['POST'])
 def record_game():
   payload = parse_json(request)
+  print('>>>>>>>{}', payload)
   data = parse_message(payload['text'])
 
   # Find Players
